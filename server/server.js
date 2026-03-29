6e421111d0df7b321636df1ec0a77c663b734b88
Ankit_cd <ankitmehta4526@gmail.com>
2026-03-29 23:35:30 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cache-Control","Expires","Pragma"],
+  }),
+);
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
