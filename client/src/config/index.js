df32546cc225383edcb355dfac0e4d5424ed4d54
Ankit_cd <ankitmehta4526@gmail.com>
2026-04-03 21:48:22 +0530

    login page added

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -22,3 +22,21 @@ export const registerFormControls = [
     }
 
 ]
+
+export const loginFormControls = [
+    {
+        name: "email",
+        label: "Email",
+        placeholder: "Enter your email",
+        type: "email",
+        componentType: "input",
+    },
+    {
+        name: "password",
+        label: "Password",
+        placeholder: "Enter your password",
+        type: "password",
+        componentType: "input",
+    }
+
+]
--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+
+import FormTemplate from '@/components/common/FormTemplate';
+import { loginFormControls } from '@/config';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 const Login = () => {
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
   return (
-    <div>Login</div>
+    <div className='mx-auto w-full max-w-md space-y-6'>
+      <div className="text-center">
+        <h1 className='text-3xl font-bol tracking-tight text-foreground'>Sign in to your account</h1>
+        <p className='mt-2'>Don't have an account? <Link to="/auth/register" className="text-primary hover:underline ml-2 font-medium">Register</Link></p>
+      </div>
+      <FormTemplate 
+      formControls={loginFormControls}
+      buttonText={'Login'}
+      formData={formData}
+      setFormData={setFormData}
+      onSubmit={onSubmit} />
+    </div>
   )
 }
 
